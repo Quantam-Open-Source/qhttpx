cfcfdc681ed1e56afd47c0a0781860e9908d666a
Dev <dev@noteal.com>
2026-01-26 12:17:33 +0200

    Fix: Add Linux and macOS platform support to core loader

--- core/index.js
+++ core/index.js
@@ -5,6 +5,14 @@ let nativeBinding = null
 
 if (platform === 'win32' && arch === 'x64') {
   nativeBinding = require('./qhttpx-core-new.win32-x64-msvc.node')
+} else if (platform === 'linux' && arch === 'x64') {
+  nativeBinding = require('./qhttpx-core-new.linux-x64-gnu.node')
+} else if (platform === 'darwin') {
+  if (arch === 'x64') {
+    nativeBinding = require('./qhttpx-core-new.darwin-x64.node')
+  } else if (arch === 'arm64') {
+    nativeBinding = require('./qhttpx-core-new.darwin-arm64.node')
+  }
 } else {
   throw new Error(`Unsupported platform: ${platform} ${arch}`)
 }
